83288dab51be445f93c95f69dae7ca34c90da8e3
chris Nwasiwe <chris.nwasiwe@gmil.com>
2026-06-23 08:55:13 +0100

    initial login page project

---
+++ script.js
@@ -0,0 +1,29 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const signinButton = document.querySelector("#signin");
+const message = document.querySelector("#message");
+const label = document.querySelector("#label");
+
+// check if the email is empty
+signinButton.addEventListener("click", function () {
+  if (email.value === "") {
+    message.textContent = "please enter your email address";
+  }
+  if (email.value === "@") {
+    message.textContent = "Provide a correct email address";
+  }
+  if (password.value === "") {
+    message.textContent = "please enter your password";
+  }
+  if (
+    email.value === "chris.nwasiwe@gmail.com" &&
+    password.value === "1234567"
+  ) {
+    message.textContent = "login successful";
+    message.style.color = "green";
+    signinButton.style.backgroundColor = "green";
+  } else {
+    message.textContent = "Invalid email or password";
+    message.style.color = "red";
+  }
+});
